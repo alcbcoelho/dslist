d7db67c83de263ddac3b38a17805d5015886cd5b
André Coêlho <alcbcoelho@gmail.com>
2024-11-13 01:28:44 -0300

    Consulta SQL, projection, busca de games por lista

---
+++ src/main/java/com/devsuperior/dslist/projections/GameMinProjection.java
@@ -0,0 +1,18 @@
+package com.devsuperior.dslist.projections;
+
+/*
+
+ Projections são interfaces que queries personalizadas como a feita no GameRepository retornam.
+ 
+ Devemos incluir nela getters para cada atributo retornado pela query.
+ 
+ */
+
+public interface GameMinProjection {
+	Long getId();
+	String getTitle();
+	Integer getYear();
+	String getImgUrl();
+	String getShortDescription();
+	Integer getPosition();
+}
--- src/main/java/com/devsuperior/dslist/services/GameService.java
+++ src/main/java/com/devsuperior/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service	// registra essa classe como sendo um componente Service do sistema
@@ -28,5 +29,11 @@ public class GameService {
 		Game game = gameRepository.findById(id).get();	// o método get é necessário p/ resgatar um objeto
 														// (não um tipo Optional<Game>, como é o retorno de findById)
 		return new GameDTO(game);
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId) {
+		List<GameMinProjection> games = gameRepository.searchByList(listId);
+		return games.stream().map(g -> new GameMinDTO(g)).toList();
 	}
 }
